514630e58d8bcac6979705fb97b71e1d2c956479
Arthur <ameliepoulain-92@mail.ru>
2016-07-30 00:43:36 +0300

    contentGrid

--- gulpfile.js
+++ gulpfile.js
@@ -3,30 +3,36 @@ var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 var gulpif = require('gulp-if');
 var minifyCSS = require('gulp-minify-css');
+var csscomb = require('gulp-csscomb');
+var sourcemaps = require('gulp-sourcemaps');
 var uglify = require('gulp-uglify');
 var prettify = require('gulp-jsbeautifier');
 var useref = require('gulp-useref');
 
 gulp.task('scss', function() {
 	return gulp.src('app/scss/*.scss')
+	.pipe(sourcemaps.init())
 	.pipe(sass().on('error', sass.logError))
 	.pipe(autoprefixer({
 		browsers: ['last 5 versions', '>0%', 'ie 7'],
 		cascade: false
 	}))
+	.pipe(csscomb())
+	.pipe(prettify())
+	.pipe(sourcemaps.write())
 	.pipe(gulp.dest('app/css'));
 });
 
-gulp.task('useref', function(){
-	return gulp.src('app/*.html')
-	.pipe(prettify())
-	.pipe(useref())
-	.pipe(gulpif('*.css', minifyCSS()))
-	.pipe(gulpif('*.js', uglify({
-		mangle: false
-	})))
-	.pipe(gulp.dest('dist/'));
-});
+// gulp.task('useref', function(){
+// 	return gulp.src('app/*.html')
+// 	.pipe(prettify())
+// 	.pipe(useref())
+// 	.pipe(gulpif('*.css', minifyCSS()))
+// 	.pipe(gulpif('*.js', uglify({
+// 		mangle: false
+// 	})))
+// 	.pipe(gulp.dest('dist/'));
+// });
 
 // gulp.task('watch', function() {
 // 	gulp.watch('app/scss/**/*.*', ['scss']);
